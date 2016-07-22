12bbc091d1075cc4d5e0eb658e3ff75fa8da60d8
bhumycka <you@example.com>
2016-07-22 21:09:02 +0000

    Styling login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
-
 //= require jquery
-
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,12 @@
 class Place < ActiveRecord::Base
-belongs_to :user
-has_many :comments
+	belongs_to :user
+	has_many :comments
 
 	geocoded_by :address
     after_validation :geocode
 
-validates :name, :length => { minimum:4 },:presence => true
-validates :address, :presence => true
-validates :description, :presence => true
+	validates :name, :length => { minimum:4 },:presence => true
+	validates :address, :presence => true
+	validates :description, :presence => true
 
 end
