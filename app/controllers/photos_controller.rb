d68fa842719dbacd64d74376bd242d55ef32db78
bhumycka <you@example.com>
2016-07-26 03:19:21 +0000

    Making the Image field work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,9 @@
-<div class="booyah-box col-xs-10 col-xs-offset-1">
 class PhotosController < ApplicationController
 	before_action :authenticate_user!
+
   def create
-  	@place = Photo.find(params[:place_id])
-  	@place.photos.create(photo_params.merge(user: current_user))
+  	@place = Place.find(params[:place_id])
+  	@place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
@@ -13,4 +13,4 @@ class PhotosController < ApplicationController
   	params.require(:photo).permit(:caption)
   end
 end
-</div>
+
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
-  validates :caption, :presence => true
-  validates :place_id, :presence => true
+  mount_uploader :picture, PictureUploader
 
 end
